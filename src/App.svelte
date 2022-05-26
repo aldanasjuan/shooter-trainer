8aade4dc210a91eaef2e4fa7446c118e5ef50f19
aldanasjuan <aldanasjuan@gmail.com>
2022-05-26 11:07:57 -0600

    crosshairs, movement, missed clicks

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,60 @@
 
 <script>
+	import {newLocalStore} from "./store"
 	import {onMount} from "svelte"
-	let screenX = 500
-	let screenY = 500
-	let size = 10
+	const defaultOptions = {
+		size: 5,
+		screenX: 200,
+		screenY: 200,
+		move: true,
+		moveSizeX: 50,
+		moveSizeY: 50,
+		moveSpeed: 600,
+		movePause: 100,
+		crosshair: 1,
+	}
+	let config = newLocalStore("config", defaultOptions)
+	let moveX = 0
+	let moveY = 0
 	let svg
 	let start, end
 	let lastResult = null
 	let dot
+	let misses = 0
+	let missesResults = []
+	let lastMisses = null
 	let results = []
 	let once = false
 	let shown = true
 	let screenW, screenH
 	let cooldown = false
-	 
-	function getAverage(values){
-		let sum = values.reduce((acc, val) => acc+val, 0)
-		return Math.round(values.length < 1 ? 0 : sum / values.length)
+	let moveInterval
+	let moveTimeout
+
+	function setDefaults(){
+		$config = defaultOptions
+		restart()
 	}
-	
+	 
+	function getAverage(values, round = true){
+		let sum = values.reduce((acc, val) => acc+val, 0)
+		if(round){
+			return Math.round(values.length < 1 ? 0 : sum / values.length)
+		}
+		return values.length < 1 ? 0 : sum / values.length
+	}
+	function handleMisses(e){
+		if(cooldown || !shown)return
+		if(e.target.id != "dot" && once){
+			misses += 1
+		}
+	}
 	function handleClick(e){
 		if(cooldown || !shown)return
 		cooldown = true
 		setTimeout(() => cooldown = false, 200)
 		let {width, height} = svg.getBoundingClientRect()
-		hide(e.target)
+		hideAndCalculate(e.target)
 		setTimeout(() => {
 			newPosition(width, height, e.target)	
 		}, random(400, 700))
@@ -33,20 +63,23 @@
 	
 	function newPosition(width, height, node){
 		show(node)
-		let x = random(10, width - size)
-		let y = random(10, height - size)
+		let x = random(10, width - $config.size - $config.moveSizeX)
+		let y = random(10, height - $config.size - $config.moveSizeY)
 		node.setAttribute("cx", x)
 		node.setAttribute("cy", y)
 	}
 	
-	function hide(node){
+	function hideAndCalculate(node){
 		shown = false
 		end = Date.now()
 		node.style.opacity = "0"
 		if(!once){
 			once = true
 		}else{
+			missesResults = [...missesResults, misses]
 			lastResult = end - start
+			lastMisses = misses
+			misses = 0
 			results = [...results, lastResult]
 		}
 	}
@@ -73,19 +106,53 @@
 	
 	function restart(){
 		lastResult = null
+		lastMisses = null
 		once = false
+		moveX = 0
+		moveY = 0
 		results = []
+		missesResults = []
 		init()
 	}
+	
 
 	function init(){
+		cleanup()
+		moveInterval = setInterval(moveAnimation, $config.moveSpeed + $config.movePause)
 		let {width, height} = svg.getBoundingClientRect()
-		dot.setAttribute("cx", (width/2) - (size/2))
-		dot.setAttribute("cy", (height/2) - (size/2))
+		dot.setAttribute("cx", (width/2) - ($config.size/2))
+		dot.setAttribute("cy", (height/2) - ($config.size/2))
 	}
+	function cleanup(){
+		clearInterval(moveInterval)
+	}
+
+	function moveAnimation(){
+		if(!$config.move || (!once)){
+			moveX = 0
+			moveY = 0
+			return
+		}
+		moveX = random(0, $config.moveSizeX)
+		moveY = random(0, $config.moveSizeY)
+	}
+	function updateInterval(speed, pause){
+		clearTimeout(moveTimeout)
+		moveTimeout = setTimeout(() => {
+			console.log("updating")
+			clearInterval(moveInterval)
+			moveInterval = setInterval(moveAnimation, speed + pause)
+		}, 750)
+	}
+
+	$:{
+		updateInterval($config.moveSpeed, $config.movePause)
+	}
+	
 
 	onMount(() => {
 		init()
+		return cleanup
 	})
 	
 </script>
@@ -93,6 +160,9 @@
 <svelte:window on:keydown={handleKey} bind:innerWidth={screenW} bind:innerHeight={screenH}/>
 
 <style>
+	* {
+		user-select: none;
+	}
 	:global(body, html), .container, svg{
 		width: 100%;
 		height: 100%;
@@ -111,7 +181,6 @@
 		display:block;
 	}
 	main{
-		
 		background-color:#ccc;
 		display: grid;
 		place-items:center;
@@ -121,12 +190,25 @@
 		line-height:1;
 		margin: 10px 0;
 		text-align:center;
-		position:absolute;
+		position:relative;
 		user-select:none;
 		color: rgb(0,0,0,0.4);
 	}
 	.result{
 		animation: out 700ms 1 forwards;
+		position:absolute;
+	}
+	.grid{
+		display:grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap:8px;
+	}
+	.crosshair > * {
+		margin-right: 16px;
+		padding: 0 8px;
+		cursor:pointer;
+		text-align:center;
+
 	}
 	
 	@keyframes out{
@@ -140,36 +222,80 @@
 </style>
 
 <div class="container">
-	<main style="width: {screenX}px;height:{screenY}px">
-	<h1>
-		{#if lastResult}
-			Avg Speed: <span style="color:rgb(255,0,0,0.4)">{getAverage(results)} </span><br/>
-		{:else}
-			Click red dot to start<br/>
-		{/if}
-		<span style="font-size:14px">
-			Screen Size: {screenX} x {screenY}<br/> 
-			Dot Size: {size} <br/>
-			- Press Esc to reset<br/>
-			- Left/Right to resize dot
-		</span>
-	</h1>
-	{#key lastResult}
-		<h1 class="result" style="top: 20%; color:rgb(255,0,0,0.4)">
-			{lastResult || ""}
-		</h1>
-	{/key}
-	<svg bind:this={svg}>
-		<circle bind:this={dot} on:click={handleClick} r={size} fill="red"/>
-	</svg>
-</main>
-	<label>Dot Size
-		<input type="number" bind:value={size}/>
-	</label>	
-	<label>Horizontal Size
-		<input type="number" bind:value={screenX}/>
-	</label>
-	<label>Vertical Size
-		<input type="number" bind:value={screenY}/>
-	</label>		
+	<main style="width: {$config.screenX}px;height:{$config.screenY}px; cursor: url('/crosshair-{[1,2,3,4].includes($config.crosshair) ? $config.crosshair : 1}.png') 12 12, auto;" on:click={handleMisses}>
+		{#key lastResult}
+			<h1 class="result" style="color:rgb(255,0,0,0.4)">
+				{lastResult || ""}{lastMisses !== null ? ` - ${lastMisses}` : ""}
+			</h1>
+		{/key}	
+		<svg bind:this={svg}>
+			<circle id="dot" style="transition:transform {$config.moveSpeed}ms ease-in-out;transform: translate({moveX}px, {moveY}px);"  bind:this={dot} on:click={handleClick} r={$config.size} fill="red"/>
+		</svg>
+	</main>
+	<div class="content">
+		<h3>
+			Avg Speed: <span style="color:rgb(255,0,0,0.4)">{lastResult ? getAverage(results) : "Click red dot to start"} </span><br/>
+		</h3>
+		<h3>
+			Missed clicks: <span style="color:rgb(255,0,0,0.4)">{lastResult ? getAverage(missesResults, false).toFixed(2) : ""} </span><br/>
+		</h3>
+		<div style="font-size:14px">
+			<p>
+				- Click red dot to start
+			</p>
+			<p>
+				- Press Esc to reset
+			</p>
+			<p>
+				- Left/Right to resize dot
+			</p>
+		</div>
+		<div class="grid">
+			<label>Dot Size
+				<input type="number" bind:value={$config.size}/>
+			</label>	
+			<label>Horizontal Size
+				<input type="number" bind:value={$config.screenX}/>
+			</label>
+			<label>Vertical Size
+				<input type="number" bind:value={$config.screenY}/>
+			</label>
+			<p style="grid-column: 1 / -1; margin: 0;">Crosshair</p>
+			<div class="crosshair" style="grid-column: 1 / -1; display: flex;">
+				<label>1
+					<input type="radio" bind:group={$config.crosshair} value={1}/>
+				</label>
+				<label>2
+					<input type="radio" bind:group={$config.crosshair} value={2}/>
+				</label>
+				<label>3
+					<input type="radio" bind:group={$config.crosshair} value={3}/>
+				</label>
+				<label>4
+					<input type="radio" bind:group={$config.crosshair} value={4}/>
+				</label>
+			</div>	
+			<label style="grid-column: 1 / -1; cursor:pointer;">Move
+				<input type="checkbox" bind:checked={$config.move}/>
+			</label>
+			{#if $config.move}
+				<label>Horizontal Move Distance
+					<input type="number" bind:value={$config.moveSizeX}/>
+				</label>	
+				<label>Vertical Move Distance
+					<input type="number" bind:value={$config.moveSizeY}/>
+				</label>	
+				<label>Move Speed
+					<input type="number" bind:value={$config.moveSpeed}/>
+				</label>	
+				<label>Move Pause
+					<input type="number" bind:value={$config.movePause}/>
+				</label>	
+			{/if}
+			<button style="grid-column: 1 / -1" on:click={setDefaults}>
+				Set to default
+			</button>
+		</div>
+	</div>
+
 </div>
